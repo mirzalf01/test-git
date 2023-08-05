8754405a234df809ad6974b28cfb1f54691be3c4
mirzalf01 <48443904+mirzalf01@users.noreply.github.com>
2023-08-05 15:06:03 +0800

    Update baru.php

--- baru.php
+++ baru.php
@@ -1,9 +1,4 @@
 <?php  
-
-	echo "Echo sabre";
-	echo "Good for sven";
-  echo "Shadow blade";
-	echo "Lothar";
+  	echo "Shadow blade";
 	echo "Shadow blade is called Lothar back in the old days";
-
 ?>
